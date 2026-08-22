96a4e942d31350fbfb61f8fbce1a0056fc87940c
kamalcodezen <kamaluddin7908@gmail.com>
2026-08-22 19:22:25 +0530

    feat: solve fibonacci problem

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -26,3 +26,33 @@ function isLeapYear(year) {
 
 
 // ``````````````````````````````````````
+
+
+
+/**
+ * 02. Generate Fibonacci Sequence Up to N Terms
+ *
+ * @param {number} n
+ * @return {number[]}
+ */
+function generateFibonacci(n) {
+    const result = [];
+
+    if (n <= 0) {
+        return result;
+    }
+
+    if (n >= 1) {
+        result.push(0);
+    }
+
+    if (n >= 2) {
+        result.push(1);
+    }
+
+    for (let i = 2; i < n; i++) {
+        result.push(result[i - 1] + result[i - 2]);
+    }
+
+    return result;
+}
