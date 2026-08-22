90aa2e9f47112faec0463a1c4caf95aa5483bc00
kamalcodezen <kamaluddin7908@gmail.com>
2026-08-22 19:28:37 +0530

    feat: solve merge sorted arrays problem

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -128,3 +128,52 @@ function isPrime(num) {
 
     return true;
 }
+
+
+
+
+
+
+// `````````````````````````````````````
+
+/**
+ * 06. Merge Two Sorted Arrays into One Sorted Array
+ *
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+function mergeSortedArrays(arr1, arr2) {
+    const result = [];
+
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] <= arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+
+
+// ````````````````````````````````````
+
+
