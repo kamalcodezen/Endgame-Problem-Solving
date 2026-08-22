75e2e587375103ce6145913544c371a57bac856e
kamalcodezen <kamaluddin7908@gmail.com>
2026-08-22 19:22:03 +0530

    feat: solve leap year problem

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -7,3 +7,22 @@
 function isLeapYear(year) {
     return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
 }
+
+
+
+// ``````````````````````````````````````
+
+
+/**
+ * 01. Check if a Year is a Leap Year
+ *
+ * @param {number} year
+ * @return {boolean}
+ */
+function isLeapYear(year) {
+    return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+}
+
+
+
+// ``````````````````````````````````````
