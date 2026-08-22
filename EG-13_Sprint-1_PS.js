fb6e2416493a870fffe170784621e2aaaff5a221
kamalcodezen <kamaluddin7908@gmail.com>
2026-08-22 19:25:48 +0530

    feat: solve prime number problem

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -94,3 +94,37 @@ function findGCD(a, b) {
 function findLCM(a, b) {
     return (a * b) / findGCD(a, b);
 }
+
+
+
+
+// ``````````````````````````````````
+
+
+/**
+ * 05. Check if a Number is Prime
+ *
+ * @param {number} num
+ * @return {boolean}
+ */
+function isPrime(num) {
+    if (num < 2) {
+        return false;
+    }
+
+    if (num === 2) {
+        return true;
+    }
+
+    if (num % 2 === 0) {
+        return false;
+    }
+
+    for (let i = 3; i * i <= num; i += 2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
