d0d1b1f9ca22fef34aa212027ca9167456cf20c5
kamalcodezen <kamaluddin7908@gmail.com>
2026-09-08 23:13:41 +0530

    check code

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -349,8 +349,6 @@ const longestConsecutive = function (nums) {
 /* =========================================================
    10. Sort Colors
    ========================================================= */
-
-   
 /**
  * Write an in-place sorting function that sorts an array
  * containing only 0, 1, and 2 so that the same colors are
